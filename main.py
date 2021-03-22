fdf79673e9a07063e7ad4d517bbd167f64d2c9e6
unknown <tkitekjamila@gmail.com>
2021-03-22 17:25:44 +0100

    Created a simple app to upload any type of file and store it under "static" folder.
    The "templates" folder will contain all our html templates.
    "templates/index.html" is the main page of our application with a form to upload any type of file and save it locally under the "static" folder.

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template, flash, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'static'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file or submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            file_path = (os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+            file.save(file_path)
+    return render_template('index.html', title='upload_file')
